9ee0887c4789f3012b96dda3b4aa652a2ad2da52
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-06-02 15:01:08 +0700

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Nguyen Quang Minh on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
